c6a2bcabc346934a23bbb7d6188b59181cdb81e6
axin <kdkt1412@outlook.com>
2026-03-25 15:22:03 +0800

    后端初始化

---
+++ src/main/java/com/axin/axinagent/AxinAgentApplication.java
@@ -0,0 +1,13 @@
+package com.axin.axinagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxinAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxinAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/axin/axinagent/AxinAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axin.axinagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxinAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
